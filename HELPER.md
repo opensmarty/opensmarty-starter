8077b2d1684c07b50e96a0f2b726cd1fba8bf05b
opensmarty <opensmarty@163.com>
2017-12-07 07:31:40 +0800

    ok, this is empty info for committing.

--- HELPER.md
+++ HELPER.md
@@ -1,5 +1,6 @@
 # Helper For OS
 This is a helpful document for your opensmarty-starter.
+
 ##Create your tag and deploy it in your git system, please.
 ```bash
 git tag v1.0.0 -m '发布源码v1.0.0版本'
@@ -16,3 +17,34 @@ git push origin --delete tag v1.0.0
 ##How do you release your tags to Github?
 ###1.点击【Release】按钮.
 ###2.如果要创建Release,则点击右上角【Draft a new release】按钮.
+
+#Upgrade php7.0 to php7.1 for your Ubuntu 16.04.
+##升级php7.1
+```bash
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+sudo apt-get upgrade php
+sudo apt-get install php7.1-mbstring
+sudo apt-get install php7.1-gd
+sudo apt-get install php7.1-dom
+sudo apt-get install php7.1-mysql
+sudo apt-get install php7.1-sqlite 
+sudo apt-get install php7.1-fpm
+sudo apt-get install php7.1-cgi
+sudo apt-get install php7.1-phpdbg
+```
+
+##phalcon重新安装，注意要兼容php7.1。
+```bash
+sudo apt-get install php7.1-dev libpcre3-dev gcc make re2c
+sudo cp /etc/php/7.0/mods-available/phalcon.ini  /etc/php/7.1/mods-available/phalcon.ini 
+sudo ln -s /etc/php/7.1/mods-available/phalcon.ini  /etc/php/7.1/apache2/conf.d/20-phalcon.ini
+sudo ln -s /etc/php/7.1/mods-available/phalcon.ini  /etc/php/7.1/fpm/conf.d/20-phalcon.ini
+sudo ln -s /etc/php/7.1/mods-available/phalcon.ini  /etc/php/7.1/cli/conf.d/20-phalcon.ini
+sudo ln -s /etc/php/7.1/mods-available/phalcon.ini  /etc/php/7.1/cgi/conf.d/20-phalcon.ini
+sudo ln -s /etc/php/7.1/mods-available/phalcon.ini  /etc/php/7.1/phpdbg/conf.d/20-phalcon.ini
+
+git clone git://github.com/phalcon/cphalcon.git
+cd cphalcon/build
+sudo ./install
+```
---
+++ git.push.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#提交说明参数，命令没带参数，则设m为空
+
+if [ -z $1 ]
+then
+    m="ok, this is empty info for committing."
+else
+    m=$1
+fi
+
+#根目录
+root=$(pwd)
+cd $root
+
+#项目名称
+#demo for github:
+#   https://github.com/opensmarty/opensmarty-demo.git
+#starter for github:
+#   https://github.com/opensmarty/opensmarty-starter.git
+# 自动化同步发布demo和starter
+# demo 用于测试及开发laravel扩展包； starter只是一个纯净的框架，禁止提交非必要文件。
+demo=${root##*/}
+starter=${demo%-*}-'starter'
+#远程分支
+
+# branch[0] 为默认的origin.
+branchName[0]="origin"
+branchName[1]="demo"
+branchName[2]="starter"
+
+branch[0]="git@github.com:opensmarty/$demo.git"
+branch[1]="git@github.com:opensmarty/$demo.git"
+branch[2]="git@github.com:opensmarty/$starter.git"
+
+print_r() {
+    index=0
+    for name in ${branch[@]}
+    do
+        echo "branch["$index"]/【${branchName[$index]}】:"$name
+        ((index++))
+    done
+}
+
+#打印项目分支
+echo ''
+echo '打印项目分支如下:'
+print_r
+
+#提交函数
+push(){
+
+    index=0
+    for val in ${branch[@]}
+    do
+        origin=${branchName[$index]}
+
+        if [[ $(git remote -v) =~ $origin ]]
+        then
+            if [[ $origin == 'origin' ]]
+            then
+                echo ''
+            else
+                echo "分支[$index]:$origin"
+                git pull $origin master
+                git add .
+                git status
+                git commit -m "$m"
+                git push $origin master
+            fi
+        fi
+
+        ((index++))
+#        echo ""
+    done
+}
+push
+#执行过程
+#echo "项目【$project】开始更新"
+#push
+#echo "更新结束"
